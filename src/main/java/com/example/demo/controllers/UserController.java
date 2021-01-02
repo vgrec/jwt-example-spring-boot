c5b4e524ddbf583b589011c7ec11b28bd31e12de
vgrec <grec.vs@gmail.com>
2021-01-02 17:57:50 +0100

    initial

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,60 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.CreateUserRequest;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.lf5.LogLevel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private Logger logger = LogManager.getLogger(UserController.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CartRepository cartRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.of(userRepository.findById(id));
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<User> findByUserName(@PathVariable String username) {
+        User user = userRepository.findByUsername(username);
+        return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+        if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        logger.debug("Creating a new user...");
+
+        User user = new User();
+        user.setUsername(createUserRequest.getUsername());
+        user.setPassword(bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+        Cart cart = new Cart();
+        cartRepository.save(cart);
+        user.setCart(cart);
+        userRepository.save(user);
+        return ResponseEntity.ok(user);
+    }
+
+}
