a0c19c6cb78021ac0c4d225a1a1844cd6cc0a283
vgrec <grec.vs@gmail.com>
2021-01-03 17:43:18 +0100

    Add unit test for user creation - success path

--- src/test/java/com/example/demo/UserControllerTests.java
+++ src/test/java/com/example/demo/UserControllerTests.java
@@ -7,25 +7,24 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 import com.example.demo.model.requests.CreateUserRequest;
 import org.junit.Before;
 import org.junit.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class UserControllerTests {
-
-    @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
-    private CartRepository cartRepository;
-
-    @MockBean
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private UserRepository userRepository = mock(UserRepository.class);
+    private CartRepository cartRepository = mock(CartRepository.class);
+    private BCryptPasswordEncoder bCryptPasswordEncoder = mock(BCryptPasswordEncoder.class);
 
     private UserController userController;
+
+    private static final String ENCODED_PASSWORD = "***encoded_password***";
 
     @Before
     public void setUp() {
@@ -33,15 +32,30 @@ public class UserControllerTests {
     }
 
     @Test
-    public void testCreateUserFailure() {
-        ResponseEntity<User> responseEntity = userController.createUser(getUser());
+    public void createUserFailure() {
+        ResponseEntity<User> responseEntity = userController.createUser(
+                getUser("password", "confirm_password")
+        );
         assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
     }
 
+    @Test
+    public void createUserSuccess() {
+        when(bCryptPasswordEncoder.encode(anyString())).thenReturn(ENCODED_PASSWORD);
 
-    private CreateUserRequest getUser() {
-        return new CreateUserRequest("lavilas", "bartovski", "cimok");
+        CreateUserRequest user = getUser("password", "password");
+        ResponseEntity<User> response = userController.createUser(user);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        User createdUser = response.getBody();
+        assertNotNull(createdUser);
+        assertEquals(user.getUsername(), createdUser.getUsername());
+        assertEquals(ENCODED_PASSWORD, createdUser.getPassword());
     }
 
+    private CreateUserRequest getUser(String password, String confirmPassword) {
+        return new CreateUserRequest("lavilas", password, confirmPassword);
+    }
 
 }
